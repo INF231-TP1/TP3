75ecd6b9a070d2e490cdc8fd3b9549f2433b58d6
Azambou Marthe Nefertyti <martheazambou@gmail.com>
2025-10-08 10:13:01 +0000

    Implémentation du négatif d'image et documentation

--- main.c
+++ main.c
@@ -39,6 +39,17 @@ int main() {
             } else {
                 printf("Erreur: Arguments manquants pour 'gris'. Utilisation: gris fichier.ppm\n");
             }
+        } else if (strncmp(command, "neg ", 4) == 0) {
+            char filename_in[100], filename_out[100];
+            if (sscanf(command + 4, "%99s %99s", filename_in, filename_out) == 2) {
+                if (negatif_ppm(filename_in, filename_out)) {
+                    printf("opération effectuée\n");
+                } else {
+                    printf("Erreur lors de la création du négatif\n");
+                }
+            } else {
+                printf("Erreur: Arguments manquants pour 'neg'. Utilisation: neg fichier.ppm fichier_resultat.ppm\n");
+            }
         }
     }
     
